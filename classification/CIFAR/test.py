26cd83bc21b7eb037f4565a03124a381a7fda052
Yang Chen <yangchen@0587669867.vpn.umich.net>
2023-11-14 22:27:00 -0500

    update data directory

--- classification/CIFAR/test.py
+++ classification/CIFAR/test.py
@@ -61,7 +61,7 @@ std = [x / 255 for x in [63.0, 62.1, 66.7]]
 test_transform = trn.Compose([trn.ToTensor(), trn.Normalize(mean, std)])
 
 if 'cifar10_' in args.method_name:
-    test_data = dset.CIFAR10('/nobackup-slow/dataset/cifarpy', train=False, transform=test_transform, download=True)
+    test_data = dset.CIFAR10('./data/cifar10', train=False, transform=test_transform, download=True)
     num_classes = 10
 else:
     test_data = dset.CIFAR100('/nobackup-slow/dataset/cifarpy', train=False, transform=test_transform, download=True)
